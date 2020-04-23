0c8647202133770d6abe1e75a6202cc2389b0720
distefano98 <54177541+distefano98@users.noreply.github.com>
2020-04-23 14:56:40 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+
+<html lang="it">
+    <head>
+        <title> NOME CATALOGO </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../CSS/style.css" type="text/css" rel="stylesheet">   
+    </head>
+   
+    
+    <body>
+        
+        <header class="header ">
+            <a href="index.html" class="header__logo" > <img class="header__logo__img" src="">  </a>
+            
+            <ul class="header__menu">
+                <li class="header__menu__item"> <a href="index.html"> HOME </a></li>
+                <li class="header__menu__item"> <a href=""> CATALOGO </a></li>
+                <li class="header__menu__item"> <a href=""> CONTATTI </a></li>
+                <li class="header__menu__item"> <a href="accedi.html"> ACCEDI </a></li>
+            </ul>
+            
+        </header>
+        
+        <section class="cover">
+            <h1> titolo di prova </h1>
+        </section>
+        
+        
+        <footer class="footer">
+            <ul class="footer__menu">
+                <li class="footer__menu__item"> <a href=""> Home </a></li>
+                <li class="footer__menu__item"> <a href=""> Chi siamo</a></li>
+                <li class="footer__menu__item"> <a href=""> Dove Siamo </a></li>
+                <li class="footer__menu__item"> <a href=""> Contattaci </a></li>
+                <li class="footer__menu__item"> <a href=""> account </a></li>
+            </ul>
+            
+        </footer>
+    </body>
+</html>
