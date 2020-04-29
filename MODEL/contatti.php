1e24a28d63d2d70956ef588c909bc9892d49e41b
luca <lucads02@libero.it>
2020-04-29 12:33:02 +0200

    riorganizzazione

--- MODEL/contatti.php
+++ MODEL/contatti.php
@@ -16,19 +16,18 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../CSS/style.css" type="text/css" rel="stylesheet">  
-        <link href="../JS/slider.js"  type="text/javascript" >
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        
     </head>
    
     
     <body>
         
         <header class="header ">
-            <a href="main.php" class="header__logo" > <img width="250" src="../UTILS/logo.png">  </a>
+            <a href="homepage.html" class="header__logo" > <img width="250" src="../UTILS/logo.png">  </a>
             
             <ul class="header__menu">
-                <li class="header__menu__item"> <a href="main.php"> HOME </a></li>
-                <li class="header__menu__item"> <a href=""> CATALOGO </a></li>
+                <li class="header__menu__item"> <a href="homepage.html"> HOME </a></li>
+                <li class="header__menu__item"> <a href="catalogo.html"> CATALOGO </a></li>
                 <li class="header__menu__item"> <a href="contatti.php"> CONTATTI </a></li>
                 <li class="header__menu__item"> <a href=""> ACCEDI </a></li>
             </ul>
@@ -36,21 +35,13 @@ and open the template in the editor.
         </header>
         
         <div class="cover">
+            <?php
             
-            <div class="cover_slider" id="slide">
-               
-                <img class="cover_slider_item" id="slide1" src="" alt="" />
-                <img class="cover_slider_item" id="slide2" src="" alt="" /> 
-               
-                <p></p>
-            </div>
-            <script src="../JS/slider.js"> </script>
-            <script>$(document).ready(function()
-                {     
-               slideshow(5,1000,4000);});
-
-            </script>
-          
+            
+            
+            
+            ?>
+            
             
         </div>
         
